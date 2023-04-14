49e0ea87a218607c49bc311f31122875468c4d85
AKDQ <quintansvedasto01>
2023-04-14 17:03:10 +0800

    add signtalk icon

--- resources/js/components/guest/About.vue
+++ resources/js/components/guest/About.vue
@@ -1,8 +1,9 @@
 <template> 
-    <div class="flex justify-center items-center h-screen w-screen bg-red-500 ">
+    <div class="flex justify-center items-center h-screen w-screen bg-red-200">
         <div class="p-4 rounded-lg w-22 border-4 border-black h-auto text-center ">
             <h1 class="text-4xl font-poppins ">Welcome to About Page!</h1>
             <router-link to="/Home" class="text-lg text-cyan-600 underline">Return Home</router-link>
+
         </div>
     </div>
 </template>
