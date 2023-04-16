3a945c50d0d55d0a50b2013ddf2959bf549670b6
AKDQ <quintansvedasto01>
2023-04-16 17:24:55 +0800

    add border bottom to topbar

--- resources/js/components/misc/TopBar.vue
+++ resources/js/components/misc/TopBar.vue
@@ -10,7 +10,7 @@ export default{
 </script>
 <template>
  
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-700">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
