f2440378f201d3fbd0f8eb2866809d89efd9fbec
Carl Mendoza <carllezter.mendoza@tup.edu.ph>
2023-05-08 22:00:17 +0800

    Hello

--- resources/js/components/misc/TopBar.vue
+++ resources/js/components/misc/TopBar.vue
@@ -25,6 +25,25 @@
               Login
             </router-link>
           </li>
+          <li>
+            <Menu as="div" class="relative">
+                <MenuButton class="-m-1.5 flex items-center p-1.5">
+                    <span class="sr-only">Open user menu</span>
+                    <img class="h-8 w-8 rounded-full bg-gray-50" src="../../../assets/images/Vedasto.png" alt="Profile" />
+                    <span class="hidden lg:flex lg:items-center">
+                    <span class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">Vedaddy</span>
+                    <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </span>
+                </MenuButton>
+                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                    <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                        <a :href="item.href" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900 capitalize']">{{ item.name }}</a>
+                    </MenuItem>
+                    </MenuItems>
+                </transition>
+                </Menu>
+          </li>
         </ul>
       </div>
     </div>
@@ -33,22 +52,40 @@
 
 <script setup>
   import { useRouter, useRoute } from 'vue-router';
-
+  import {
+  Dialog,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
 </script>
 
 <script>
+      const userNavigation = [
+  { name: 'My profile', href: '#' } ,
+  { name: 'My Analytics', href: '#' } ,
+  { name: 'Settings', href: '#' } ,
+  { name: 'Sign out', href: '#' },
+]
 export default {
 
 
   data() {
     return {
+
+
       topbarNavigation:[
         { name: 'Home', href: '/student/home' },
         { name: 'Dictionary', href: '/student/dictionary'},
         { name: 'Lesson', href: '/student/lesson' },
         { name: 'Quiz', href: '/student/quiz' },
       ],
-      open: false
+      open: false,
+      
     }
   },
 
