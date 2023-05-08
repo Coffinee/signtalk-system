4220dedadd3f6b8ed63149c84e809032aa95e175
Intern User <intern1@globalland.com.ph>
2023-05-08 16:24:09 +0800

    Started Profile Page

--- resources/js/components/misc/TopBar.vue
+++ resources/js/components/misc/TopBar.vue
@@ -5,7 +5,7 @@
           <p class="self-center text-3xl font-bold whitespace-nowrap dark:text-white">Sign<span
           class="text-indigo-500  font-bold">Talk</span></p>
       </router-link>
-      <button @click="expand"  type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
+      <button @click="expand"     type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" >
         <span class="sr-only">Open main menu</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
@@ -25,6 +25,24 @@
               Login
             </router-link>
           </li>
+          <li>
+            <Menu as="div" class="relative">
+                <MenuButton class="-m-1.5 flex items-center p-1.5">
+                    <span class="sr-only">Open user menu</span>
+                    <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    <span class="hidden lg:flex lg:items-center">
+                    <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </span>
+                </MenuButton>
+                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                    <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                        <a :href="item.href" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900 capitalize']">{{ item.name }}</a>
+                    </MenuItem>
+                    </MenuItems>
+                </transition>
+            </Menu>
+          </li>
         </ul>
       </div>
     </div>
@@ -33,15 +51,31 @@
 
 <script setup>
   import { useRouter, useRoute } from 'vue-router';
-
+  import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+  TransitionRoot,
+} from '@headlessui/vue'
 </script>
 
 <script>
+const userNavigation = [
+  { name: 'My Profile', href: '#' },
+  { name: 'My Analytics', href: '#' },
+  { name: 'My Settings', href: '#' },
+  { name: 'Logout', href: '#' },
+]
 export default {
+
 
 
   data() {
     return {
+
+
       topbarNavigation:[
         { name: 'Home', href: '/index' },
         { name: 'Dictionary', href: '/student/dictionary'},
