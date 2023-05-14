255f46fde5f95ebd883e7e3df180988cd19db4f8
GlennRoh <glennrossmata078@gmail.com>
2023-05-14 15:26:16 +0800

    glenn

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import { createApp } from 'vue';
 import Router from "@/router";
 
 const app = createApp({});
+
 app.use(Router);
 app.mount('#app');
+
 
---
+++ resources/js/components/testfile.vue
@@ -0,0 +1,15 @@
+<!-- for testing only -->
+
+<template>
+
+</template>
+
+<script>
+
+export default {
+    components:{
+        
+    }
+}
+
+</script>
