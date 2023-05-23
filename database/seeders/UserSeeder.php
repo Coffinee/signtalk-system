6b7583f2b98158a16884e327394aefd3e55cb0ac
Intern User <intern1@globalland.com.ph>
2023-05-23 16:48:01 +0800

    Added Modal IN teacher Layout, fixed seeders, added New Settings

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,35 +16,35 @@ class UserSeeder extends Seeder
      */
     public function run():void{
         $user = DB::table('users')->insert([
-            [   
-            'role' => 'admin',         
-            'first_name' => 'Vedasto',  
-            'last_name' => 'Quintans',           
-            'email' => 'quintansvedasto@gmail.com',          
-            'password' => Hash::make('asdasd123'),          
-            'email_verified_at' => date('Y-m-d h:i:s'),         
+            [
+            'role' => 'admin',
+            'first_name' => 'Vedasto',
+            'last_name' => 'Quintans',
+            'email' => 'quintansvedasto@gmail.com',
+            'password' => Hash::make('asdasd123'),
+            'email_verified_at' => date('Y-m-d h:i:s'),
             'created_at' => date('Y-m-d h:i:s'),
             ],
-            [   
-                'role' => 'student',         
-                'first_name' => 'Glenn',  
-                'last_name' => 'Mata',           
-                'email' => 'glenn@gmail.com',          
-                'password' => Hash::make('asdasd123'),          
-                'email_verified_at' => date('Y-m-d h:i:s'),         
+            [
+                'role' => 'student',
+                'first_name' => 'Glenn Ross',
+                'last_name' => 'Mata',
+                'email' => 'glenn@gmail.com',
+                'password' => Hash::make('asdasd123'),
+                'email_verified_at' => date('Y-m-d h:i:s'),
                 'created_at' => date('Y-m-d h:i:s'),
             ],
-            [   
-                'role' => 'teacher',         
-                'first_name' => 'Carl',  
-                'last_name' => 'Mendosa',           
-                'email' => 'carl@gmail.com',          
-                'password' => Hash::make('asdasd123'),          
-                'email_verified_at' => date('Y-m-d h:i:s'),         
+            [
+                'role' => 'teacher',
+                'first_name' => 'Carl Lezter',
+                'last_name' => 'Mendoza',
+                'email' => 'carl@gmail.com',
+                'password' => Hash::make('asdasd123'),
+                'email_verified_at' => date('Y-m-d h:i:s'),
                 'created_at' => date('Y-m-d h:i:s'),
             ],
     ]);
-        
+
     }
-    
+
 }
--- resources/js/components/AuthenticatedPages/Teacher/TeacherClasses/TeacherClasses.vue
+++ resources/js/components/AuthenticatedPages/Teacher/TeacherClasses/TeacherClasses.vue
@@ -5,7 +5,7 @@
                 <h2 class="text-2xl font-extrabold font-poppins">My Classes</h2>
             </div>
             <div>
-                <button
+                <button @click="(modalOpen = !modalOpen)"
                     class="hover:text-white hover:bg-indigo-500 rounded-md border-2 border-indigo-500 text-indigo-500 text-base w-[150px] h-auto py-1 px-3">Add
                     Class</button>
             </div>
@@ -14,7 +14,7 @@
             <TabGroup :selectedIndex="selectedTab" @change="changeTab">
                 <TabList
                     class=" justify-start flex text-sm font-medium text-center dark:text-gray-400 dark:border-gray-700 w-full mt-[20px] mb-[20px] border-b space-x-1">
-                    <Tab v-for="tab in settingTabs" as="template" :key="tab" v-slot="{ selected }">
+                    <Tab v-for="tab in SectionList" as="template" :key="tab" v-slot="{ selected }">
                         <button :class="[
                             'w-[150px] rounded-t-lg p-2 text-sm font-medium focus:outline-none text-black hover:bg-indigo-500 hover:text-white',
                             selected
@@ -25,7 +25,6 @@
                         </button>
                     </Tab>
                 </TabList>
-
                 <TabPanels>
                     <TabPanel class="h-full w-full border border-black rounded-md p-4 space-y-5">
                         <Student1/>
@@ -40,24 +39,40 @@
             </TabGroup>
         </div>
     </div>
+
+    <Modal :show="modalOpen" @close="modalToggle" :title="'Create A New Section'" :heightModal="'h-[150px]'">
+        <div class="w-full h-full">
+            <div class="space-y-1">
+                <label for="word" class="text-[15px]">Enter Your Class Section: </label>
+                <input type="text" class="focus:outline-none pl-2 text-xs w-full h-8 rounded-md border border-indigo-900">
+            </div>
+            <div class="flex justify-center mt-5 gap-5">
+                <button @click="(modalOpen = !modalOpen)"
+                    class="hover:text-white hover:bg-[#3E3E3E] rounded-md border-2 border-[#3E3E3E] text-#3E3E3E text-base w-[150px] h-auto py-1 px-3 uppercase">CANCEL</button>
+                <button @click="(modalOpen = !modalOpen)" class="hover:text-white hover:bg-indigo-500 rounded-md border-2 border-indigo-500 text-indigo-500 text-base w-[150px] h-auto py-1 px-3 uppercase">ADD</button>
+            </div>
+        </div>
+    </Modal>
 </template>
 <script>
 
 import Student1 from './Tabs/Student1.vue';
+import Modal from '../../../misc/Modal.vue';
 
 export default {
     components:{
         Student1,
-
+        Modal
     },
     data() {
         return {
-            settingTabs: [
+            SectionList: [
                 { name: 'Section 1' },
                 { name: 'Section 2' },
                 { name: 'Section 3' },
             ],
-            textToCopy: ''
+            textToCopy: '',
+            modalOpen: false,
 
         }
     },
@@ -66,6 +81,9 @@ export default {
         copy() {
             this.$refs.clone.focus();
             document.execCommand('copy');
+        },
+        modalToggle() {
+            this.modalOpen = false;
         },
     },
 
