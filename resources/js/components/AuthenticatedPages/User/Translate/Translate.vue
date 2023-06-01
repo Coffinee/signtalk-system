a28d5d1ea436afa785563493e7bb39523cc6f625
quintansvedasto01 <quintansvedasto01@gmail.com>
2023-06-01 09:20:18 +0800

    for merge

--- resources/js/components/AuthenticatedPages/User/Translate/Translate.vue
+++ resources/js/components/AuthenticatedPages/User/Translate/Translate.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="flex justify-center items-center h-screen">
         <div
-            class="w-full md:w-[70%] lg:w-[50%] h-[80%] md:h-[70%] bg-white dark:bg-gray-800 dark:border-gray-700 shadow shadow-indigo-300 rounded-lg">
+            class="w-full md:w-[70%] h-[80%] md:h-[90%] bg-white dark:bg-gray-800 dark:border-gray-700 shadow shadow-indigo-300 rounded-lg">
             <div class="flex flex-col space-y-3 m-3 h-[95%] p-5">
                 <!-- result -->
-                <div class="w-full h-full flex items-center justify-center flex-wrap gap-[5px] border border-gray-400 rounded-md overflow-auto">
+                <div class="w-full h-full flex items-center justify-center flex-wrap  gap-[5px] border border-gray-400 rounded-md overflow-auto">
                     <div v-for="(letter, index) in valueArray" :key="index">
                         <img :src="getImagePath(letter)" class="h-[60px] w-[60px] sm:h-[60px] sm:w-[60px] md:h-[120px] md:w-[120px]">              
                     </div>
                 </div>
                 <!-- end result -->
                 <div class="flex space-x-3">
-                    <input v-model="value" type="text" placeholder="Type your message ..." 
-                        class="w-full h-10 rounded-full border border-gray-400 pl-5 bg-gray-100 text-black dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400">
+                    <!-- <input v-model="value" type="text" placeholder="Type your message ..." 
+                        class="w-full h-10 rounded-full border border-gray-400 pl-5 bg-gray-100 text-black dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400"> -->
+                    
+                    <textarea  v-model="value"  id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Input words to translate..."></textarea>
+
+                    
                     <!-- <span>
                         <button 
                             class="btn-mic flex items-center justify-center border border-gray-400 rounded-full h-10 w-10 hover:bg-gray-100">
